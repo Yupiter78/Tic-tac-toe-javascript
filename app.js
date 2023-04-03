3989cb03869d3a45ac677d44c419879c7d6e762b
Yuriy Vedernikov <yuriy.jsx@gmail.com>
2023-04-03 23:42:51 +0300

    feat: add new functionality tic-tac-toe javascript

--- app.js
+++ app.js
@@ -4,9 +4,28 @@ const infoDisplay = document.querySelector("#info");
 
 const startCells = Array(9).fill("");
 
+let isCircle = false;
+
+function handlerEvent(event, cellElement) {
+    let element = document.createElement("div");
+    element.classList.add(`${event}`);
+    cellElement.append(element);
+    isCircle = !isCircle;
+}
+
 function createBoard(initialCells) {
     initialCells.forEach((cell, index) => {
         const cellElement = document.createElement("div")
-    })
+        cellElement.classList.add("square");
+
+        cellElement.onclick = () => {
+            log("cellElement.firstChild:", cellElement.firstChild);
+
+            if (cellElement.firstChild) return;
+            isCircle ? handlerEvent("cross", cellElement) : handlerEvent("circle", cellElement)
+        }
+        gameBoard.append(cellElement);
+    });
 }
 
+createBoard(startCells);
