1e04230818c3a17ccf5edff3ac4f6a11ac4ed901
Yuriy Vedernikov <yuriy.jsx@gmail.com>
2023-04-04 23:21:56 +0300

    feat: add new functionality tic-tac-toe javascript

--- app.js
+++ app.js
@@ -6,33 +6,71 @@ const startCells = Array(9).fill("");
 const resultCellsMap = new Map(Object.entries(Array(9).fill("")));
 
 let isCircle = false;
+let isGameOver = false;
 
-function handlerEvent(figure, cellElement) {
-    let element = document.createElement("div");
-    element.classList.add(`${figure}`);
-    cellElement.append(element);
-    resultCellsMap.set(cellElement.num, figure);
-    log(resultCellsMap);
-    if (new Set([resultCellsMap.get("0"), resultCellsMap.get("1"), resultCellsMap.get("2")]).size === 1) {
-        const h1 = document.createElement("h1");
-        h1.textContent = "YOU WINNER!";
-        document.body.append(h1);
+function startGame() {
+    function createBoard(initialCells) {
+        initialCells.forEach((cell, index) => {
+            const cellElement = document.createElement("div")
+            cellElement.classList.add("square");
+            cellElement.num = `${index}`;
+
+            cellElement.onclick = () => {
+                log("CLICK");
+                log("isCircle:", isCircle);
+                log("cellElement.num :", cellElement.num);
+
+                isCircle ? handlerEvent?.("red-circle", cellElement) : handlerEvent?.("blue-circle", cellElement);
+                cellElement.onclick = null;
+            }
+            gameBoard.append(cellElement);
+        });
     }
-    isCircle = !isCircle;
-}
 
-function createBoard(initialCells) {
-    initialCells.forEach((cell, index) => {
-        const cellElement = document.createElement("div")
-        cellElement.classList.add("square");
-        cellElement.num = `${index}`;
+    function createWinnerMessage(typeWinner) {
+        const h1 = document.createElement("h1");
+        h1.textContent = `${typeWinner.toUpperCase()} YOU WINNER!`;
+        document.body.append(h1);
+        handlerEvent = null;
+    }
 
-        cellElement.onclick = () => {
-            cellElement.onclick = null;
-            isCircle ? handlerEvent("cross", cellElement) : handlerEvent("circle", cellElement);
+    function handlerEvent(figure, cell) {
+        let element = document.createElement("div");
+        element.classList.add(`${figure}`);
+        cell.append(element);
+        resultCellsMap.set(cell.num, figure);
+        log(resultCellsMap);
+        switch (1) {
+            case new Set([resultCellsMap.get("0"), resultCellsMap.get("1"), resultCellsMap.get("2")]).size:
+                if (resultCellsMap.get("0") !== "") createWinnerMessage(resultCellsMap.get("0"));
+                break;
+            case new Set([resultCellsMap.get("3"), resultCellsMap.get("4"), resultCellsMap.get("5")]).size:
+                if (resultCellsMap.get("3") !== "") createWinnerMessage(resultCellsMap.get("3"));
+                break;
+            case new Set([resultCellsMap.get("6"), resultCellsMap.get("7"), resultCellsMap.get("8")]).size:
+                if (resultCellsMap.get("6") !== "") createWinnerMessage(resultCellsMap.get("6"));
+                break;
+            case new Set([resultCellsMap.get("0"), resultCellsMap.get("3"), resultCellsMap.get("6")]).size:
+                if (resultCellsMap.get("0") !== "") createWinnerMessage(resultCellsMap.get("0"));
+                break;
+            case new Set([resultCellsMap.get("1"), resultCellsMap.get("4"), resultCellsMap.get("7")]).size:
+                if (resultCellsMap.get("1") !== "") createWinnerMessage(resultCellsMap.get("1"));
+                break;
+            case new Set([resultCellsMap.get("2"), resultCellsMap.get("5"), resultCellsMap.get("8")]).size:
+                if (resultCellsMap.get("2") !== "") createWinnerMessage(resultCellsMap.get("2"));
+                break;
+            case new Set([resultCellsMap.get("0"), resultCellsMap.get("4"), resultCellsMap.get("8")]).size:
+                if (resultCellsMap.get("0") !== "") createWinnerMessage(resultCellsMap.get("0"));
+                break;
+            case new Set([resultCellsMap.get("2"), resultCellsMap.get("4"), resultCellsMap.get("6")]).size:
+                if (resultCellsMap.get("2") !== "") createWinnerMessage(resultCellsMap.get("2"));
+                break;
+
         }
-        gameBoard.append(cellElement);
-    });
+        isCircle = !isCircle;
+    }
+
+    createBoard(startCells);
 }
 
-createBoard(startCells);
+startGame();
