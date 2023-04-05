489854ea24a2c096ae62960d4cbe668b93a3c2c7
Yuriy Vedernikov <yuriy.jsx@gmail.com>
2023-04-06 01:06:17 +0300

    feat: add new functionality tic-tac-toe javascript

--- app.js
+++ app.js
@@ -3,74 +3,69 @@ const gameBoard = document.querySelector("#gameboard");
 const infoDisplay = document.querySelector("#info");
 
 const startCells = Array(9).fill("");
-const resultCellsMap = new Map(Object.entries(Array(9).fill("")));
 
-let isCircle = false;
-let isGameOver = false;
+let go = "circle";
+infoDisplay.textContent = "Circle`s start the Game";
 
-function startGame() {
-    function createBoard(initialCells) {
-        initialCells.forEach((cell, index) => {
-            const cellElement = document.createElement("div")
-            cellElement.classList.add("square");
-            cellElement.num = `${index}`;
+function createBoard(initialCells) {
+    initialCells.forEach((_cell, index) => {
+        const cellElement = document.createElement("div")
+        cellElement.classList.add("square");
+        cellElement.id = index;
 
-            cellElement.onclick = () => {
-                log("CLICK");
-                log("isCircle:", isCircle);
-                log("cellElement.num :", cellElement.num);
-
-                isCircle ? handlerEvent?.("red-circle", cellElement) : handlerEvent?.("blue-circle", cellElement);
-                cellElement.onclick = null;
-            }
-            gameBoard.append(cellElement);
-        });
-    }
-
-    function createWinnerMessage(typeWinner) {
-        const h1 = document.createElement("h1");
-        h1.textContent = `${typeWinner.toUpperCase()} YOU WINNER!`;
-        document.body.append(h1);
-        handlerEvent = null;
-    }
-
-    function handlerEvent(figure, cell) {
-        let element = document.createElement("div");
-        element.classList.add(`${figure}`);
-        cell.append(element);
-        resultCellsMap.set(cell.num, figure);
-        log(resultCellsMap);
-        switch (1) {
-            case new Set([resultCellsMap.get("0"), resultCellsMap.get("1"), resultCellsMap.get("2")]).size:
-                if (resultCellsMap.get("0") !== "") createWinnerMessage(resultCellsMap.get("0"));
-                break;
-            case new Set([resultCellsMap.get("3"), resultCellsMap.get("4"), resultCellsMap.get("5")]).size:
-                if (resultCellsMap.get("3") !== "") createWinnerMessage(resultCellsMap.get("3"));
-                break;
-            case new Set([resultCellsMap.get("6"), resultCellsMap.get("7"), resultCellsMap.get("8")]).size:
-                if (resultCellsMap.get("6") !== "") createWinnerMessage(resultCellsMap.get("6"));
-                break;
-            case new Set([resultCellsMap.get("0"), resultCellsMap.get("3"), resultCellsMap.get("6")]).size:
-                if (resultCellsMap.get("0") !== "") createWinnerMessage(resultCellsMap.get("0"));
-                break;
-            case new Set([resultCellsMap.get("1"), resultCellsMap.get("4"), resultCellsMap.get("7")]).size:
-                if (resultCellsMap.get("1") !== "") createWinnerMessage(resultCellsMap.get("1"));
-                break;
-            case new Set([resultCellsMap.get("2"), resultCellsMap.get("5"), resultCellsMap.get("8")]).size:
-                if (resultCellsMap.get("2") !== "") createWinnerMessage(resultCellsMap.get("2"));
-                break;
-            case new Set([resultCellsMap.get("0"), resultCellsMap.get("4"), resultCellsMap.get("8")]).size:
-                if (resultCellsMap.get("0") !== "") createWinnerMessage(resultCellsMap.get("0"));
-                break;
-            case new Set([resultCellsMap.get("2"), resultCellsMap.get("4"), resultCellsMap.get("6")]).size:
-                if (resultCellsMap.get("2") !== "") createWinnerMessage(resultCellsMap.get("2"));
-                break;
-
-        }
-        isCircle = !isCircle;
-    }
-
-    createBoard(startCells);
+        cellElement.addEventListener("click", addGo);
+        gameBoard.append(cellElement);
+    })
 }
 
-startGame();
+function addGo(event) {
+    const goDisplay = document.createElement("div");
+    goDisplay.classList.add(go);
+    event.target.append(goDisplay);
+
+    go = go === "circle" ? "cross" : "circle";
+    event.target.removeEventListener("click", addGo);
+    infoDisplay.textContent = `it is now ${go}'s go.`;
+
+    checkScore();
+
+}
+
+function checkScore() {
+    const winnerCombos = [
+        [0, 1, 2], [3, 4, 5], [6, 7, 8],
+        [0, 3, 6], [1, 4, 7], [2, 5, 8],
+        [0, 4, 8], [2, 4, 6]
+    ];
+
+    const addSquares = document.querySelectorAll(".square");
+    // winnerCombos.forEach(array => {
+    //     const winner = array.every(cell => addSquares[cell].firstChild?.classList.contains(go));
+    //     log("go:", go, "winner:", winner);
+    //     if (winner) {
+    //         infoDisplay.textContent = `WINNER ${go}!`;
+    //         return;
+    //     }
+    // })
+    winnerCombos.forEach(array => {
+        const circleWinner = array.every(cell => addSquares[cell].firstChild?.classList.contains("circle"));
+        if (circleWinner) {
+            infoDisplay.textContent = "Circle Win!"
+            addSquares.forEach(square => square.replaceWith(square.cloneNode(true)));
+            return null;
+        }
+    });
+
+    winnerCombos.forEach(array => {
+        const circleWinner = array.every(cell => addSquares[cell].firstChild?.classList.contains("cross"));
+        if (circleWinner) {
+            infoDisplay.textContent = "Cross Win!"
+            addSquares.forEach(square => square.replaceWith(square.cloneNode(true)));
+            return null;
+        }
+    });
+
+}
+
+
+createBoard(startCells);
